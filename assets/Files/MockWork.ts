e51e8fb2bafda75d603b0eef6cbc008d22d6b3b8
Ojedajuliano <jno_978@hotmail.com>
2023-05-15 22:00:26 -0300

    Auto-generated commit [ci skip]

---
+++ assets/Files/MockWork.ts
@@ -0,0 +1,21 @@
+import { WorkModel } from "src/app/models/work.model";
+
+export const WORK:WorkModel[]= [{
+    "id":1,
+    "title":"1er trabajo",
+    "description":"Me encanto este trabajo",
+    "urlImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4d6HhQIVYWcf74LZEZEutP4XTVOw4TvpCZA&usqp=CAU",
+  },
+  {
+    "id":2,
+    "title":"Segundo trabajo",
+    "description":"Me encanto este trabajo",
+    "urlImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4d6HhQIVYWcf74LZEZEutP4XTVOw4TvpCZA&usqp=CAU",
+  },
+  {
+    "id":3,
+    "title":"Tercer trabajo",
+    "description":"Me encanto este trabajo",
+    "urlImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4d6HhQIVYWcf74LZEZEutP4XTVOw4TvpCZA&usqp=CAU",
+  }]
+
